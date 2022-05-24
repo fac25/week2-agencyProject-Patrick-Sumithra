df5e2a5407eda440badad972969914d0a408918a
Dogwish <zefiru@protonmail.com>
2022-05-24 20:38:03 +0100

    Add form basic validation with use of `required` in HTML

--- scripts/Contact/Form.js
+++ scripts/Contact/Form.js
@@ -1,12 +1,16 @@
 const formItemNodeList = document.querySelectorAll(".form__item");
 
-formItemNodeList.forEach((item) =>
-  item.addEventListener("click", handleFormItemClick)
-);
+formItemNodeList.forEach((item) => {
+  item.addEventListener("click", handleFormItemClick);
+});
 
 function handleFormItemClick({ currentTarget }) {
-  formItemNodeList.forEach((item) =>
-    item.classList.remove("form__item--active")
-  );
+  formItemNodeList.forEach((item) => {
+    // Validate if field is empty
+    const currentInput = item.querySelector(".form__input");
+    if (currentInput.value.length > 0) return;
+
+    item.classList.remove("form__item--active");
+  });
   currentTarget.classList.add("form__item--active");
 }
