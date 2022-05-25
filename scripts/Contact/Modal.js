866c6852c22882e6b6037950687f606b78916893
Dogwish <zefiru@protonmail.com>
2022-05-26 00:52:20 +0100

    Add functions to hide modal when click outside the div

--- scripts/Contact/Modal.js
+++ scripts/Contact/Modal.js
@@ -1,8 +1,20 @@
+const body = document.querySelector("body");
 const contactBtn = document.querySelector("button");
 const modal = document.querySelector(".modal");
 
+body.addEventListener("click", hideForm);
+modal.addEventListener("click", modalStopPropagation);
 contactBtn.addEventListener("click", displayForm);
 
-function displayForm() {
+function displayForm(event) {
+  event.stopPropagation();
   modal.classList.toggle("modal--active");
+}
+
+function modalStopPropagation(event) {
+  event.stopPropagation();
+}
+
+function hideForm() {
+  modal.classList.remove("modal--active");
 }
