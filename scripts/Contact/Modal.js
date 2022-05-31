021f298575d75c19afced7940430bd6abbc77b3d
Dogwish <zefiru@protonmail.com>
2022-05-31 03:50:02 +0100

    Refactor file to allow event propagation #18

--- scripts/Contact/Modal.js
+++ scripts/Contact/Modal.js
@@ -2,19 +2,14 @@ const body = document.querySelector("body");
 const contactBtn = document.querySelector("button");
 const modal = document.querySelector(".modal");
 
-body.addEventListener("click", hideForm);
-modal.addEventListener("click", modalStopPropagation);
+document.addEventListener("click", hideForm);
 contactBtn.addEventListener("click", displayForm);
 
-function displayForm(event) {
-  event.stopPropagation();
+function displayForm() {
   modal.classList.toggle("modal--active");
 }
 
-function modalStopPropagation(event) {
-  event.stopPropagation();
-}
-
-function hideForm() {
+function hideForm({ target }) {
+  if (modal.contains(target) || target === contactBtn) return;
   modal.classList.remove("modal--active");
 }
